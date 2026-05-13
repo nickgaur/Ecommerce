f157f5a3d66ce7fae4d04e1989c78f05d681d692
nick gaur <67472332+nickgaur@users.noreply.github.com>
2026-05-14 01:14:38 +0530

    Implementing custom authentication

--- src/main/java/com/ecommerce/project/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -6,7 +6,9 @@ import com.ecommerce.project.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
     Page<Product> findProductByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
---
+++ src/main/java/com/ecommerce/project/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.ecommerce.project.security;
+
+import com.ecommerce.project.security.jwt.AuthEntryPointJwt;
+import com.ecommerce.project.security.jwt.AuthTokenFilter;
+import com.ecommerce.project.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+//@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setUserDetailsPasswordService(userDetailsService);
+    }
+
+}
